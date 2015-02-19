fe4a354f9c294c48364802d32a104fc23563b5f2
Mohamed Mahrous <m.mahrous.94@gmail.com>
2015-02-19 14:49:57 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Mesaha Booking System</title>
+	<link rel="stylesheet" type="text/css" href="css/foundation.css">
+</head>
+<body>
+<script type="text/javascript" src="js/foundation.min.js"></script>
+<div class="row">
+  <div class="large-3 columns"></div>
+  <div class="large-6 columns"><img src="img/logo.jpg"></div>
+  <div class="large-3 columns"><a href="#">Login</a> . <a href="#">Registreation</a></div>
+</div>
+</body>
+</html>
